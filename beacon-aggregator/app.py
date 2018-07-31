353bf212cd20f91e11a34d9959aaff4afe5cb537
Teemu Kataja <kataja.teemu@gmail.com>
2018-07-31 13:27:27 +0300

    Update app.py

--- beacon-aggregator/app.py
+++ beacon-aggregator/app.py
@@ -90,9 +90,11 @@ async def query(beacon, q):
     async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar) as session:
         try:
             cookies = session.cookie_jar.filter_cookies(os.environ.get('COOKIE_DOMAIN', None))
-            for cookie in cookies:
+            access_token = cookies['access_token'].value
+            LOG.info(cookies.keys())
+            '''for cookie in cookies:
                 if cookie.key == 'access_token':
-                    access_token = cookie.value
+                    access_token = cookie.value'''
             async with session.get(beacon,
                                    params=q,
                                    ssl=os.environ.get('HTTPS_ONLY', True),
