f24c9426ecca65248f9707a81a99ad5f34fc328b
teemukataja <kataja.teemu@gmail.com>
2018-08-09 13:44:10 +0300

    modified mock endpoints to return response according to beacon 1.0 specification

--- beacon-mockup/app.py
+++ beacon-mockup/app.py
@@ -11,17 +11,95 @@ CORS(app)
 
 @app.route("/api/true")
 def mock_true():
-    return jsonify({'beacon': 'Always True', 'response': True, 'query': request.args})
+    return jsonify({'beaconId': 'Always True',
+                    'apiVersion': '1.0.0',
+                    'exists': True,
+                    'error': None,
+                    'alleleRequest': {'referenceName': request.args.get('referenceName'),
+                                      'start': request.args.get('start'),
+                                      'startMin': request.args.get('startMin'),
+                                      'startMax': request.args.get('startMax'),
+                                      'end': request.args.get('end'),
+                                      'endMin': request.args.get('endMin'),
+                                      'endMax': request.args.get('endMax'),
+                                      'referenceBases': request.args.get('referenceBases'),
+                                      'alternateBases': request.args.get('alternateBases'),
+                                      'variantType': request.args.get('variantType'),
+                                      'assemblyId': request.args.get('assemblyId'),
+                                      'datasetIds': request.args.get('datasetIds'),
+                                      'includeDatasetResponses': request.args.get('includeDatasetsResponses')},
+                    'datasetAlleleResponses': [{'datasetId': 'EGAD000000000001',
+                                                'exists': True,
+                                                'frequency': 0.5,
+                                                'variantCount': 1,
+                                                'callCount': 5000,
+                                                'sampleCount': 2500,
+                                                'note': 'Mockup dataset',
+                                                'externalUrl': None,
+                                                'info': {'accessType': 'PUBLIC'},
+                                                'error': None}]})
 
 
 @app.route("/api/false")
 def mock_false():
-    return jsonify({'beacon': 'Always False', 'response': False, 'query': request.args})
+    return jsonify({'beaconId': 'Always False',
+                    'apiVersion': '1.0.0',
+                    'exists': False,
+                    'error': None,
+                    'alleleRequest': {'referenceName': request.args.get('referenceName'),
+                                      'start': request.args.get('start'),
+                                      'startMin': request.args.get('startMin'),
+                                      'startMax': request.args.get('startMax'),
+                                      'end': request.args.get('end'),
+                                      'endMin': request.args.get('endMin'),
+                                      'endMax': request.args.get('endMax'),
+                                      'referenceBases': request.args.get('referenceBases'),
+                                      'alternateBases': request.args.get('alternateBases'),
+                                      'variantType': request.args.get('variantType'),
+                                      'assemblyId': request.args.get('assemblyId'),
+                                      'datasetIds': request.args.get('datasetIds'),
+                                      'includeDatasetResponses': request.args.get('includeDatasetsResponses')},
+                    'datasetAlleleResponses': [{'datasetId': 'EGAD000000000002',
+                                                'exists': False,
+                                                'frequency': 0,
+                                                'variantCount': 0,
+                                                'callCount': 0,
+                                                'sampleCount': 0,
+                                                'note': 'Mockup dataset',
+                                                'externalUrl': None,
+                                                'info': {'accessType': 'PUBLIC'},
+                                                'error': None}]})
 
 
 @app.route("/api/null")
 def mock_null():
-    return jsonify({'beacon': 'Always Null', 'response': None, 'query': request.args})
+    return jsonify({'beaconId': 'Always True',
+                    'apiVersion': '1.0.0',
+                    'exists': None,
+                    'error': None,
+                    'alleleRequest': {'referenceName': request.args.get('referenceName'),
+                                      'start': request.args.get('start'),
+                                      'startMin': request.args.get('startMin'),
+                                      'startMax': request.args.get('startMax'),
+                                      'end': request.args.get('end'),
+                                      'endMin': request.args.get('endMin'),
+                                      'endMax': request.args.get('endMax'),
+                                      'referenceBases': request.args.get('referenceBases'),
+                                      'alternateBases': request.args.get('alternateBases'),
+                                      'variantType': request.args.get('variantType'),
+                                      'assemblyId': request.args.get('assemblyId'),
+                                      'datasetIds': request.args.get('datasetIds'),
+                                      'includeDatasetResponses': request.args.get('includeDatasetsResponses')},
+                    'datasetAlleleResponses': [{'datasetId': 'EGAD000000000000',
+                                                'exists': None,
+                                                'frequency': 0,
+                                                'variantCount': 0,
+                                                'callCount': 0,
+                                                'sampleCount': 0,
+                                                'note': 'Mockup dataset',
+                                                'externalUrl': None,
+                                                'info': {'accessType': 'PUBLIC'},
+                                                'error': None}]})
 
 
 if __name__ == "__main__":
